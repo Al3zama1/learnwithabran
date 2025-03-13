52149e17bf9b394f6dcbbdbc4ee75ff3a1e55198
Abran Lezama <lezama.abran@gmail.com>
2025-03-12 18:13:37 -0700

    Created client and backend projects

---
+++ learn-withabran-backend/src/main/java/com/withabran/learn/learn_withabran_backend/LearnWithabranBackendApplication.java
@@ -0,0 +1,13 @@
+package com.withabran.learn.learn_withabran_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWithabranBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnWithabranBackendApplication.class, args);
+	}
+
+}
---
+++ learn-withabran-backend/src/test/java/com/withabran/learn/learn_withabran_backend/LearnWithabranBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withabran.learn.learn_withabran_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWithabranBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
