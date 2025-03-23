828e3441396be2e8192f468bc4a2f9b3bbaa6153
Abran Lezama <lezama.abran@gmail.com>
2025-03-23 10:55:19 -0700

    Added application Header component

---
+++ learn-withabran-client/src/components/Header.tsx
@@ -0,0 +1,48 @@
+import { Menu, Moon, Search, User } from 'lucide-react'
+import useAppContext from '../hooks/UseAppContext'
+
+type Props = {
+    open: boolean,
+    setOpen: (value: boolean) => void
+}
+
+const Header = () => {
+
+    const { sidebarOpen, setSidebarOpen } = useAppContext();
+
+    const handleTheme = () => {
+        const theme = localStorage.getItem('theme')
+        const rootNode = document.getElementById("root");
+
+        if (!rootNode) return;
+        if (!theme || theme === 'dark') {
+            rootNode.classList.remove('dark')
+            localStorage.setItem('theme', 'light')
+        }  else {
+            rootNode.classList.add('dark');
+            localStorage.setItem('theme', 'dark')
+        }
+    }
+    
+  return (
+    <header className={`flex justify-around w-full dark:bg-dark-background items-center h-15 fixed
+        ${sidebarOpen === false ? 'sm:flex-col sm:h-screen sm:w-15 sm:justify-start sm:gap-10' 
+        : 
+        'sm:w-80 border-b-[1px] dark:border-dark-border '}`}>
+        <button className={`hover:cursor-pointer ${!sidebarOpen && 'sm:mt-5'}`} onClick={() => setSidebarOpen(!sidebarOpen)}>
+            <Menu />
+        </button>
+        <button className='hover:cursor-pointer' onClick={handleTheme}>
+            <Moon />
+        </button>
+        <button className='hover:cursor-pointer'>
+            <Search />
+        </button>
+        <button className='hover:cursor-pointer'>
+            <User />
+        </button>
+    </header>
+  )
+}
+
+export default Header
