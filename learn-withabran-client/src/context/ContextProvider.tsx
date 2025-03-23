2e1ba658afe3f8153f84c7bfe81a367d8b93c117
Abran Lezama <lezama.abran@gmail.com>
2025-03-23 10:54:40 -0700

    Added base application context

---
+++ learn-withabran-client/src/context/ContextProvider.tsx
@@ -0,0 +1,20 @@
+import React, { createContext, ReactNode, useState } from 'react'
+
+type ContextType = {
+    sidebarOpen: boolean,
+    setSidebarOpen: (value : boolean) => void
+}
+
+const AppContext = createContext<ContextType | null> (null);
+
+export const AppProvider = ({ children } : {children: ReactNode}) => {
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
+  
+  return (
+    <AppContext.Provider value={{ sidebarOpen, setSidebarOpen }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export default AppContext;
