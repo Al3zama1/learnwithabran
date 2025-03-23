3f52a3ebcf7aa35d0476a427234c5487548439c7
Abran Lezama <lezama.abran@gmail.com>
2025-03-23 10:39:36 -0700

    Application Router/Context Support
    Added support for react-router-dom and made an application context
    available.

--- learn-withabran-client/src/main.tsx
+++ learn-withabran-client/src/main.tsx
@@ -1,10 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout.tsx'
+import HomePage from './routes/HomePage.tsx'
+import { AppProvider } from './context/ContextProvider.tsx'
+import WritePage from './routes/WritePage.tsx'
 import App from './App.tsx'
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/:slug',
+        element: <HomePage />
+      },
+      {
+        path: '/new-story',
+        element: <WritePage />
+      }
+    ]
+  }
+])
+
+const setTheme = () => {
+  const theme = localStorage.getItem('theme');
+  const rootNode = document.getElementById('root');
+  
+  if (!rootNode) return
+  if (!theme || theme === 'dark') rootNode.classList.add('dark')
+  
+}
+
+setTheme()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProvider>
+      <RouterProvider router={router} />
+    </AppProvider>
   </StrictMode>,
 )
